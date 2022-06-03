745f35b2bd248b14880bcae5cf5995ca512e8941
Nak14 <nickkraut@gmail.com>
2022-06-03 15:46:45 -0700

    Create function to make new grid and reset grid to desired size

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+const gridContainer = document.querySelector('.container')
 
+//generate a square grid with side lengths of gridLength
+function makeGrid(gridLength) {
+
+//this loop creates the skelton for the grip
+for (let i = 0; i < gridLength; i++){
+    const newDiv = document.createElement('div')
+    gridContainer.appendChild(newDiv).classList.add('column')
+    
+    //this loop actually creates the boxes
+    for (let j=0; j < gridLength; j++){
+        const newRow = document.createElement('div')
+        const columns = document.querySelector('.column')
+        newDiv.appendChild(newRow).classList.add('row')
+       
+    }
+}
+}
+
+//to clear the grid we need to clear all the divs labeled column
+
+function resetGrid(){
+    while (gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.firstChild)
+    }
+    //turn this prompt into a button eventually
+    let newGridLength = prompt('what size grid?')
+    makeGrid(newGridLength)
+}
